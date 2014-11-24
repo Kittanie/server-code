205b08d6fd121a3a27307bd374933dc7b269bc97
Ivy <ivy.davenport@hotmail.co.uk>
2014-11-24 16:14:38 +0000

    banner test

--- wordpress-themes/felixroad/header.php
+++ wordpress-themes/felixroad/header.php
@@ -23,7 +23,7 @@
  
 
 <div class="container center-block">
-  <img src="http://felixroad.didesignbristol.com/wp-content/uploads/2014/11/home-banner.png" style="width: 1000px, height: 200px;">
+  <img class="img-responsive" src="http://felixroad.didesignbristol.com/wp-content/uploads/2014/11/home-banner.png" style="width: 1000px, height: 200px;">
 </div>
 
 
